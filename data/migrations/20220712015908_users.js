332140dd73d7de615d6a6f564532abbbbe9d06ad
Rodolfo Jaspe <68761286+RodolfoJaspe@users.noreply.github.com>
2024-08-05 19:45:49 -0400

    increased bio characters

--- data/migrations/20220712015908_users.js
+++ data/migrations/20220712015908_users.js
@@ -8,7 +8,7 @@
             table.increments("user_id");
             table.string("username", 128).notNullable().unique();
             table.string("password", 128).notNullable();
-            table.string("biography", 2000);
+            table.string("biography", 4000);
         })
         .createTable('headshots', table => {
             table.increments("id");
